0666530aded46132d19f6ccac265351280f21ff7
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-13 00:24:11 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,17 @@
+def 'Finding data'
+   scale = [2,3,4,5]
+  scale_2 = [6,5,4,3]
+  hill = [1,2,3,4,5,4,3,2,1]
+  valley = [5,4,3,2,1,0,1,2,3,4,5]
+  zig_zag = [500,4,1000,5,250]
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ find_element_index
 end
 
 def find_max_value(array)
-  # Add your solution here
+find_max_value
 end
 
 def find_min_value(array)
-  # Add your solution here
+  find_min_value
 end
