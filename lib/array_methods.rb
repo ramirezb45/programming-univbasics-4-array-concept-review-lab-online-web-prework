607322fa69bf95024823115f29e4e2e4de5bf47b
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-13 00:23:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,13 +5,13 @@ def 'Finding data'
   valley = [5,4,3,2,1,0,1,2,3,4,5]
   zig_zag = [500,4,1000,5,250]
 def find_element_index(array, value_to_find)
-
+ find_element_index
 end
 
 def find_max_value(array)
-
+find_max_value
 end
 
 def find_min_value(array)
-  # Add your solution here
+  find_min_value
 end
